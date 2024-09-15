01e46641cbdd8a750246721f586a04f8ce2db029
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-14 23:04:03 -0300

    configurando cors

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ import express from "express";
 import routesUser from "./routes/user.routes.js";
 import routesProducts from "./routes/products.routes.js";
 import routerCart from "./routes/cart.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors())
+
 app.use(routesUser);
-app.use(routesProducts)
-app.use(routerCart)
+app.use(routesProducts);
+app.use(routerCart);
 
 export default app;
