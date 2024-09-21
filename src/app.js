a67d28854915780fe3cec5a8e4898f6a2ff95564
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-21 10:49:18 -0300

    detalhando configuração de cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use(routesUser);
 app.use(routesProducts);
